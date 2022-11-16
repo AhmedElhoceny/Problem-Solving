51867f4bc211ca0fd181ddb1ff5c16de2c3a14cc
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-11-16 12:51:46 +0200

    is it Rated Problem

---
+++ Problems/A.Is It Rated/is_it_rated_Solve.cs
@@ -0,0 +1,50 @@
+﻿namespace ProblemSolving.A.Is_It_Rated
+{
+    public class is_it_rated_Solve
+    {
+        public void SolvingProblem()
+        {
+            var collaporatorsCount = Console.ReadLine().Split("").Select(x => long.Parse(x)).ToArray()[0];
+            var checkingIsRating = new List<List<long>>();
+            var collaporatorsRates = new List<long>();
+            var checkingOrderingList = new List<long>();
+            long counter = 0;
+            for (int i = 0; i < collaporatorsCount; i++)
+            {
+                var collaporatorRate = Console.ReadLine().Split(" ").Select(x => long.Parse(x)).ToList();
+                checkingIsRating.Add(collaporatorRate.Distinct().ToList());
+                collaporatorsRates.AddRange(collaporatorRate);
+            }
+            if (checkingIsRating.Where(x => x.Count == 2).FirstOrDefault() != null)
+                Console.WriteLine("rated");
+            else
+            {
+                foreach (var item in collaporatorsRates)
+                {
+
+                    var LastItem = checkingOrderingList.LastOrDefault();
+                    if (LastItem != null && LastItem != 0)
+                    {
+                        if (LastItem < item)
+                        {
+                            Console.WriteLine("unrated");
+                            break;
+                        }
+                        else
+                        {
+                            checkingOrderingList.Add(item);
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        checkingOrderingList.Add(item);
+                        counter++;
+                    }
+                }
+            }
+            if (counter == collaporatorsRates.Count)
+                Console.WriteLine("maybe");
+        }
+    }
+}
