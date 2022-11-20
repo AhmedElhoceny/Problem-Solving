5dc8a0d978d9f7390e11634618cb6d7629e8c717
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-11-20 15:08:28 +0200

    Football

---
+++ Problems/Football/Football_Solve.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems.Football
+{
+    public class Football_Solve
+    {
+        public void Solve()
+        {
+            var count = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray()[0];
+            var FirstTeam = 0;
+            var secondTeam = 0;
+            var firstName = "";
+            var secondName = "";
+            for (int i = 0; i < count; i++)
+            {
+
+                if (i == 0)
+                {
+                    FirstTeam++;
+                    firstName = Console.ReadLine();
+                }
+
+                else
+                {
+                    var checkingName = Console.ReadLine();
+                    if (checkingName == firstName)
+                    {
+                        FirstTeam++;
+                    }
+                    else
+                    {
+                        secondTeam++;
+                        secondName = checkingName;
+                    }
+                }
+            }
+            if (FirstTeam > secondTeam)
+                Console.WriteLine(firstName);
+            else
+                Console.WriteLine(secondName);
+        }
+    }
+}
