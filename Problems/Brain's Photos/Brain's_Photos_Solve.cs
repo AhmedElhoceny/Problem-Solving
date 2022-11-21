56f2c52228a91a01e6d9711d4a5530803238dc53
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-11-21 13:40:31 +0200

    Brain_s_Photos

---
+++ Problems/Brain's Photos/Brain's_Photos_Solve.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems.Brain_s_Photos
+{
+    public class Brain_s_Photos_Solve
+    {
+        public void Solve()
+        {
+            var columns = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray()[0];
+            var colors = new List<string>();
+            for (int i = 0; i < columns; i++)
+            {
+                var row = Console.ReadLine().Split(" ").ToList();
+                colors.AddRange(row);
+            }
+            colors.Distinct();
+            if (colors.All(x => x == "W" || x == "G" || x == "B"))
+                Console.WriteLine("#Black&White");
+            else
+                Console.WriteLine("#Color");
+        }
+    }
+}
