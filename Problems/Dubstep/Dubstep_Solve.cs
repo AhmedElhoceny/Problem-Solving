6fed28115f48e3e49be75598824e889071cae326
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-11-22 10:10:59 +0200

    var encriptedText = Console.ReadLine();

---
+++ Problems/Dubstep/Dubstep_Solve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems.Dubstep
+{
+    public class Dubstep_Solve
+    {
+        public void Solve()
+        {
+            var encriptedText = Console.ReadLine();
+            var Song = "";
+            var chars = encriptedText.Split("WUB").ToList();
+            foreach (var item in chars)
+            {
+                Song += item + " ";
+            }
+            Console.WriteLine(Song.Trim());
+        }
+    }
+}
