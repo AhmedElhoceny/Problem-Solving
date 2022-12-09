e592af2e38c0e8fb3267165161514227245147da
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-12-09 22:25:34 +0200

    AddAlgorithms2

---
+++ Problems/Second Order Statistics/Solution.cs
@@ -0,0 +1,25 @@
+﻿using ProblemSolving.Algorithms.Sorting;
+
+namespace ProblemSolving.Problems.Second_Order_Statistics
+{
+    public class Solution
+    {
+        void Solve()
+        {
+
+            var Count = Console.ReadLine();
+            var Input = Console.ReadLine()!.Split(" ").Select(x => double.Parse(x)).ToArray();
+            Input = Input.Distinct().ToArray();
+            if (Input.Count() == 1)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                var SortingAlgo = new MergeSort();
+                Input = SortingAlgo.SortArray(Input, 0, Input.Length - 1);
+                Console.WriteLine(Input[1]);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
-﻿using ProblemSolving.Algorithms.Sorting;
-
-var Count = Console.ReadLine();
-var Input = Console.ReadLine()!.Split(" ").Select(x => double.Parse(x)).ToArray();
-Input = Input.Distinct().ToArray();
-if(Input.Count() == 1)
-{
-    Console.WriteLine("NO");
-}
-else
-{
-    var SortingAlgo = new MergeSort();
-    Input = SortingAlgo.SortArray(Input, 0, Input.Length - 1);
-    Console.WriteLine(Input[1]);
-}
+﻿
