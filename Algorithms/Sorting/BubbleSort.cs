f9db8053fc5b3b855dbd4226b1156f6c9a4cec4b
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-12-09 22:19:19 +0200

    AddAlgorithms

---
+++ Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Algorithms.Sorting
+{
+    public class BubbleSort
+    {
+        public int[] SortArray(int[] Array)
+        {
+            var n = Array.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (Array[j] > Array[j + 1])
+                    {
+                        var tempVar = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = tempVar;
+                    }
+            return Array;
+        }
+    }
+}
---
+++ Algorithms/Sorting/selectionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Algorithms.Sorting
+{
+    public class selectionSort
+    {
+        public int[] SortArray(int[] Array)
+        {
+            var arrayLength = Array.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                var smallestVal = i;
+                for (int j = i + 1; j < arrayLength; j++)
+                {
+                    if (Array[j] < Array[smallestVal])
+                    {
+                        smallestVal = j;
+                    }
+                }
+                var tempVar = Array[smallestVal];
+                Array[smallestVal] = Array[i];
+                Array[i] = tempVar;
+            }
+            return Array;
+        }
+    }
+}
