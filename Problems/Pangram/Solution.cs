021edfe09936eccf19565c9a23c223e9259df091
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2022-12-16 00:27:49 +0200

    Pangram

---
+++ Problems/Pangram/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems.Pangram
+{
+    public class Solution
+    {
+        public void Solve()
+        {
+            var searchingArray = new int[26];
+            var charLength = Console.ReadLine()!.Split("").Select(x => int.Parse(x)).ToArray()[0];
+            var inputChar = Console.ReadLine();
+
+            if (charLength < 26)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                foreach (var inputCharItem in inputChar.ToUpper())
+                {
+                    searchingArray[((int)inputCharItem - (int)'A')] = 1;
+                }
+                if (searchingArray.Where(x => x != 1).Count() != 0)
+                    Console.WriteLine("NO");
+                else
+                    Console.WriteLine("YES");
+            }
+
+        }
+    }
+}
