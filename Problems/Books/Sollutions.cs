dc61703e3ed2e3647df7f9c613d65735a8ee3e30
ahmed elhoceny <ahmedelhoceny741@gmail.com>
2023-01-10 11:35:05 +0200

    books

---
+++ Problems/Books/Sollutions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.Problems.Books
+{
+    public class Sollutions
+    {
+        public void Solve()
+        {
+            // Collecting The Data
+            var firstDataInput = Console.ReadLine()!.Split(" ").Select(x => long.Parse(x)).ToArray();
+            var secondDataInput = Console.ReadLine()!.Split(" ").Select(x => long.Parse(x)).ToList();
+
+            // Books Counter
+            long booksCounter = 0;
+
+            // Sorting The Minutes
+            secondDataInput.Sort();
+
+            // Reading The Books Inside The Free Minutes
+            foreach (var BookMinuteItem in secondDataInput)
+            {
+                firstDataInput[1] -= BookMinuteItem;
+                if (firstDataInput[1] >= 0)
+                    booksCounter++;
+                else
+                    break;
+            }
+
+            //Print The Count Of Books
+            Console.WriteLine(booksCounter);
+        }
+    }
+}
